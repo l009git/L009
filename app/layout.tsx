8716393c6336064b4579efbf0aacf07862f61899
Daniel Mazzeu <danmzzeu9@gmail.com>
2025-11-06 23:36:59 -0300

    Upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import { metadataObject } from "./metatags";
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import "./globals.css";
 import Protection from '@/components/Protection/Protection';
+
+import "./globals.css";
 
 export const metadata: Metadata = metadataObject;
 
