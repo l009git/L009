de2d156eb415f4c11276394a1a7c64a3c261d7a3
Daniel Mazzeu <danmzzeu9@gmail.com>
2025-11-06 23:26:54 -0300

    Upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,26 @@
 import { metadataObject } from "./metatags"; 
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
-import Protection from '@/components//Protection/Protection';
+import Protection from '@/components/Protection/Protection';
 
 export const metadata: Metadata = metadataObject;
 
 export default function RootLayout({
-  children,
+  children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-BR">
-      <body>
-        <Protection>
-          {children}
-        </Protection>
-      </body>
-    </html>
-  );
+  return (
+    <html lang="pt-BR">
+      <body>
+        <Protection>
+          {children}
+        </Protection>
+        <Analytics />
+        <SpeedInsights />
+      </body>
+    </html>
+  );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'randomuser.me',
-        port: '',
-        pathname: '/api/**',
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
