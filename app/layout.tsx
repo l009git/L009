2b2186615c87d42a91bc5f2267594f53b94a1667
Daniel Mazzeu <danmzzeu9@gmail.com>
2025-11-07 23:23:51 -0300

    Upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <meta name="viewport" content="width=device-width, initial-scale=0.95, maximum-scale=0.95, user-scalable=no" />
       </head>
       <body>
         <Protection>
