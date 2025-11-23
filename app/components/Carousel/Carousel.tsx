94f63db1c23d634b207237e532010f6b0e25b573
Daniel Mazzeu <danmzzeu9@gmail.com>
2025-11-23 04:18:48 -0300

    Upload

--- app/components/Carousel/Carousel.tsx
+++ app/components/Carousel/Carousel.tsx
@@ -1,28 +1,29 @@
-import Image from 'next/image';
+/* eslint-disable @next/next/no-img-element */
+
 import style from './Carousel.module.css';
 
 const Carousel = function() {
     return (
         <div className={style.carousel}>
             <div className={style.group}>
-                <div><Image src='/images/carousel_flutter.png' alt='Logo Flutter' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
-                <div><Image src='/images/carousel_next.png' alt='Logo Next' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
-                <div><Image src='/images/carousel_react.png' alt='Logo React' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
-                <div><Image src='/images/carousel_php.png' alt='Logo PHP' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
-                <div><Image src='/images/carousel_mysql.png' alt='Logo Mysql' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
-                <div><Image src='/images/carousel_python.png' alt='Logo Python' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
-                <div><Image src='/images/carousel_n8n.png' alt='Logo n8n' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
-                <div><Image src='/images/carousel_git.png' alt='Logo Git Hub' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
+                <div><img src='/images/carousel_flutter.png' alt='Logo Flutter' /></div>
+                <div><img src='/images/carousel_next.png' alt='Logo Next' /></div>
+                <div><img src='/images/carousel_react.png' alt='Logo React' /></div>
+                <div><img src='/images/carousel_php.png' alt='Logo PHP' /></div>
+                <div><img src='/images/carousel_mysql.png' alt='Logo Mysql' /></div>
+                <div><img src='/images/carousel_python.png' alt='Logo Python' /></div>
+                <div><img src='/images/carousel_n8n.png' alt='Logo n8n' /></div>
+                <div><img src='/images/carousel_git.png' alt='Logo Git Hub' /></div>
             </div>
             <div aria-hidden className={style.group}>
-                <div><Image src='/images/carousel_flutter.png' alt='Logo Flutter' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
-                <div><Image src='/images/carousel_next.png' alt='Logo Next' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
-                <div><Image src='/images/carousel_react.png' alt='Logo React' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
-                <div><Image src='/images/carousel_php.png' alt='Logo PHP' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
-                <div><Image src='/images/carousel_mysql.png' alt='Logo Mysql' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
-                <div><Image src='/images/carousel_python.png' alt='Logo Python' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
-                <div><Image src='/images/carousel_n8n.png' alt='Logo n8n' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
-                <div><Image src='/images/carousel_git.png' alt='Logo Git Hub' width={200} height={50} style={{ objectFit: 'contain' }} priority /></div>
+                <div><img src='/images/carousel_flutter.png' alt='Logo Flutter' /></div>
+                <div><img src='/images/carousel_next.png' alt='Logo Next' /></div>
+                <div><img src='/images/carousel_react.png' alt='Logo React' /></div>
+                <div><img src='/images/carousel_php.png' alt='Logo PHP' /></div>
+                <div><img src='/images/carousel_mysql.png' alt='Logo Mysql' /></div>
+                <div><img src='/images/carousel_python.png' alt='Logo Python' /></div>
+                <div><img src='/images/carousel_n8n.png' alt='Logo n8n' /></div>
+                <div><img src='/images/carousel_git.png' alt='Logo Git Hub' /></div>
             </div>
         </div>
     );
